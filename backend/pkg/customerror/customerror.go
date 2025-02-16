1408d512a659a946983ce665665afdbce4d86e73
Jonathan Imin <jonathanimin13@gmail.com>
2025-02-16 17:56:38 +0700

    feat: setup error wrapping

---
+++ backend/pkg/customerror/customerror.go
@@ -0,0 +1,81 @@
+package customerror
+
+type customError struct {
+	Field    string
+	Sentinel error
+	Actual   error
+}
+
+func (e *customError) Error() string {
+	return e.Actual.Error()
+}
+
+type BadRequestError struct {
+	customError
+}
+
+func NewBadRequestError(field string, sentinel error, actual error) *BadRequestError {
+	return &BadRequestError{
+		customError: customError{
+			Field:    field,
+			Sentinel: sentinel,
+			Actual:   actual,
+		},
+	}
+}
+
+type UnauthorizedError struct {
+	customError
+}
+
+func NewUnauthorizedError(field string, sentinel error, actual error) *UnauthorizedError {
+	return &UnauthorizedError{
+		customError: customError{
+			Field:    field,
+			Sentinel: sentinel,
+			Actual:   actual,
+		},
+	}
+}
+
+type ForbiddenError struct {
+	customError
+}
+
+func NewForbiddenError(field string, sentinel error, actual error) *ForbiddenError {
+	return &ForbiddenError{
+		customError: customError{
+			Field:    field,
+			Sentinel: sentinel,
+			Actual:   actual,
+		},
+	}
+}
+
+type NotFoundError struct {
+	customError
+}
+
+func NewNotFoundError(field string, sentinel error, actual error) *NotFoundError {
+	return &NotFoundError{
+		customError: customError{
+			Field:    field,
+			Sentinel: sentinel,
+			Actual:   actual,
+		},
+	}
+}
+
+type InternalServerError struct {
+	customError
+}
+
+func NewInternalServerError(field string, sentinel error, actual error) *InternalServerError {
+	return &InternalServerError{
+		customError: customError{
+			Field:    field,
+			Sentinel: sentinel,
+			Actual:   actual,
+		},
+	}
+}

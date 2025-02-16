bcfc6365e717e9f7451470a1fbb26e1030f2b7b3
Jonathan Imin <jonathanimin13@gmail.com>
2025-02-17 01:28:48 +0700

    feat: implement auth repo method for login

--- backend/internal/auth/repo/repo.go
+++ backend/internal/auth/repo/repo.go
@@ -1,8 +1,17 @@
 package repo
 
-import "database/sql"
+import (
+	"auth-app/internal/apperrors"
+	"auth-app/internal/entity"
+	"auth-app/pkg/customerror"
+	"context"
+	"database/sql"
+)
 
-type AuthRepo interface{}
+type AuthRepo interface{
+	IsEmailExists(ctx context.Context, email string) (bool, error)
+	FindUserByEmail(ctx context.Context, email string) (*entity.User, error)
+}
 
 type authRepoImpl struct {
 	db *sql.DB
@@ -12,4 +21,50 @@ func NewAuthRepo(db *sql.DB) AuthRepo {
 	return &authRepoImpl{
 		db: db,
 	}
+}
+
+func (r *authRepoImpl) IsEmailExists(ctx context.Context, email string) (bool, error) {
+	query := `SELECT EXISTS (
+							SELECT 
+								1
+							FROM 
+								users
+							WHERE 
+								email = $1
+							AND 
+								deleted_at IS NULL
+						)`
+
+	var isExists bool
+	err := r.db.QueryRowContext(ctx, query, email).Scan(&isExists)
+	if err != nil {
+		return false, customerror.NewInternalServerError(apperrors.FieldServer, apperrors.ErrInternalServer, err)
+	}
+
+	return isExists, nil
+}
+
+func (r *authRepoImpl) FindUserByEmail(ctx context.Context, email string) (*entity.User, error) {
+	query := `SELECT
+							id,
+							username,
+							password
+						FROM
+							users
+						WHERE 
+							email = $1
+						AND 
+							deleted_at IS NULL`
+
+	var user entity.User
+	err := r.db.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Password,
+	)
+	if err != nil {
+		return nil, customerror.NewInternalServerError(apperrors.FieldServer, apperrors.ErrInternalServer, err)
+	}
+
+	return &user, nil
 }

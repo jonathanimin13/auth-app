64f5ed838a6e805f6fd5435893cd4fa21ab9e6d3
Jonathan Imin <jonathanimin13@gmail.com>
2025-02-16 17:50:07 +0700

    feat: add connection to db

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"auth-app/internal/infrastructure/db"
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("env: %v", err)
+	}
+
+	db, err := db.ConnectDB()
+	if err != nil {
+		log.Fatalf("db: %v", err)
+	}
+	defer db.Close()
 }
